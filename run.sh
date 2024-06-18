1dc96868d02143b28510fd0d33f7cf1d5d60bc2c
heeraknpsingh <heerasingh.mca07@gmail.com>
2024-06-18 20:06:35 +0400

    sh changes

--- run.sh
+++ run.sh
@@ -1,14 +1,13 @@
-kill -9 $(lsof -t -i:3000) &&
-npm run health &&
-kill -9 $(lsof -t -i:3000) &&
-npm run auth &&
-kill -9 $(lsof -t -i:3000) &&
-npm run book-negative &&
-kill -9 $(lsof -t -i:3000) &&
-npm run book-positive &&
-kill -9 $(lsof -t -i:3000) &&
-npm run book-schema &&
-kill -9 $(lsof -t -i:3000) &&
-npm run user-registration &&
-kill -9 $(lsof -t -i:3000) &&
-npm run user-login
+npm run health
+# kill -9 $(lsof -t -i:3000)
+# npm run auth &&
+# kill -9 $(lsof -t -i:3000) &&
+# npm run book-negative &&
+# kill -9 $(lsof -t -i:3000) &&
+# npm run book-positive &&
+# kill -9 $(lsof -t -i:3000) &&
+# npm run book-schema &&
+# kill -9 $(lsof -t -i:3000) &&
+# npm run user-registration &&
+# kill -9 $(lsof -t -i:3000) &&
+# npm run user-login
