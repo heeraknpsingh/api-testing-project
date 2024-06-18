1d8568fa585e843565f7f672f52f6aa803db9ea8
heeraknpsingh <heerasingh.mca07@gmail.com>
2024-06-18 19:02:00 +0400

    adding files

--- run.sh
+++ run.sh
@@ -1,14 +1,21 @@
-kill -9 `lsof -i TCP:3000 | awk '/LISTEN/{print $2}'` &  npm run serve-json & npm run test-health
-kill -9 888
-kill -9 `lsof -i TCP:3000 | awk '/LISTEN/{print $2}'` &  npm run serve-json-auth & npm run test-auth
-kill -9 888
-kill -9 `lsof -i TCP:3000 | awk '/LISTEN/{print $2}'` &  npm run serve-json-duplicate-entry  & npm run test-search-books-negative
-kill -9 888
-kill -9 `lsof -i TCP:3000 | awk '/LISTEN/{print $2}'` &  npm run serve-json & npm run test-search-books-positive
-kill -9 888
-kill -9 `lsof -i TCP:3000 | awk '/LISTEN/{print $2}'` &  npm run serve-json & npm run test-search-books-schema
-kill -9 888
-kill -9 `lsof -i TCP:3000 | awk '/LISTEN/{print $2}'` &  npm run serve-json-register  & npm run test-user-registration
-kill -9 888
-kill -9 `lsof -i TCP:3000 | awk '/LISTEN/{print $2}'` &  npm run serve-json-register  & npm run test-user-login
-kill -9 888
+kill -9 $(lsof -t -i:3000) &  npm run serve-json & npm run test-health &
+pid1=$!
+wait $pid1
+kill -9 $(lsof -t -i:3000) &  npm run serve-json-auth & npm run test-auth &
+pid2=$!
+wait $pid2
+kill -9 $(lsof -t -i:3000) &  npm run serve-json-duplicate-entry  & npm run test-search-books-negative &
+pid3=$!
+wait $pid3
+kill -9 $(lsof -t -i:3000) &  npm run serve-json & npm run test-search-books-positive &
+pid4=$!
+wait $pid4
+kill -9 $(lsof -t -i:3000) &  npm run serve-json & npm run test-search-books-schema &
+pid5=$!
+wait $pid5
+kill -9 $(lsof -t -i:3000) &  npm run serve-json-register  & npm run test-user-registration &
+pid6=$!
+wait $pid6
+kill -9 $(lsof -t -i:3000) &  npm run serve-json-register  & npm run test-user-login &
+pid7=$!
+wait $pid7
