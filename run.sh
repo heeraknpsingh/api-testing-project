68502fcd46389c8c5b111d9a4f28f99f94cc1f6e
heeraknpsingh <heerasingh.mca07@gmail.com>
2024-06-18 19:27:15 +0400

    sh changes

--- run.sh
+++ run.sh
@@ -1,21 +1,7 @@
-kill -9 $(lsof -t -i:3000) &  npm run serve-json & npm run test-health &
-pid1=$!
-wait $pid1
-kill -9 $(lsof -t -i:3000) &  npm run serve-json-auth & npm run test-auth &
-pid2=$!
-wait $pid2
-kill -9 $(lsof -t -i:3000) &  npm run serve-json-duplicate-entry  & npm run test-search-books-negative &
-pid3=$!
-wait $pid3
-kill -9 $(lsof -t -i:3000) &  npm run serve-json & npm run test-search-books-positive &
-pid4=$!
-wait $pid4
-kill -9 $(lsof -t -i:3000) &  npm run serve-json & npm run test-search-books-schema &
-pid5=$!
-wait $pid5
-kill -9 $(lsof -t -i:3000) &  npm run serve-json-register  & npm run test-user-registration &
-pid6=$!
-wait $pid6
-kill -9 $(lsof -t -i:3000) &  npm run serve-json-register  & npm run test-user-login &
-pid7=$!
-wait $pid7
+(kill -9 $(lsof -t -i:3000) && npm run health) &&
+(kill -9 $(lsof -t -i:3000) && npm run auth) &&
+(kill -9 $(lsof -t -i:3000) && npm run book-negative) &&
+(kill -9 $(lsof -t -i:3000) && npm run book-positive) &&
+(kill -9 $(lsof -t -i:3000) && npm run book-schema) &&
+(kill -9 $(lsof -t -i:3000) && npm run user-registration) &&
+(kill -9 $(lsof -t -i:3000) && npm run user-login)
