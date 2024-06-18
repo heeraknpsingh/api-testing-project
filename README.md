5c3f8c273c6f23e18d22392ca0c7fa8c1e0b8c60
heeraknpsingh <heerasingh.mca07@gmail.com>
2024-06-18 22:07:20 +0400

    adding files (#2)
    * adding files
    
    * sh changes
    
    * sh changes
    
    * sh changes
    
    * sh changes
    
    * sh changes
    
    * removed unwanted files
    
    * readme file

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # api-testing-project
 This repository to test login, registration, search books etc using json-server and creating different mocks and testing them
+
+# installation and Usages
+## Clone the project
+> clone the project by running command `git clone git@github.com:heeraknpsingh/api-testing-project.git`
+
+## Go inside the folder
+> Run command `cd api-testing-project`
+
+## Go inside the folder
+> Run command `sh run.sh`
+
+
+
+
+
+
--- run.sh
+++ run.sh
@@ -1,14 +1,13 @@
-kill -9 `lsof -i TCP:3000 | awk '/LISTEN/{print $2}'` &  npm run serve-json & npm run test-health
-kill -9 888
-kill -9 `lsof -i TCP:3000 | awk '/LISTEN/{print $2}'` &  npm run serve-json-auth & npm run test-auth
-kill -9 888
-kill -9 `lsof -i TCP:3000 | awk '/LISTEN/{print $2}'` &  npm run serve-json-duplicate-entry  & npm run test-search-books-negative
-kill -9 888
-kill -9 `lsof -i TCP:3000 | awk '/LISTEN/{print $2}'` &  npm run serve-json & npm run test-search-books-positive
-kill -9 888
-kill -9 `lsof -i TCP:3000 | awk '/LISTEN/{print $2}'` &  npm run serve-json & npm run test-search-books-schema
-kill -9 888
-kill -9 `lsof -i TCP:3000 | awk '/LISTEN/{print $2}'` &  npm run serve-json-register  & npm run test-user-registration
-kill -9 888
-kill -9 `lsof -i TCP:3000 | awk '/LISTEN/{print $2}'` &  npm run serve-json-register  & npm run test-user-login
-kill -9 888
+npm run health &&
+kill -9 $(lsof -t -i:3000) &&
+npm run auth &&
+kill -9 $(lsof -t -i:3000) &&
+npm run book-negative &&
+kill -9 $(lsof -t -i:3000) &&
+npm run book-positive &&
+kill -9 $(lsof -t -i:3000) &&
+npm run book-schema &&
+kill -9 $(lsof -t -i:3000) &&
+npm run user-registration &&
+kill -9 $(lsof -t -i:3000) &&
+npm run user-login
